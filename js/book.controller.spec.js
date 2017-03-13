1577f4f182182bc05deac6f28f78d19782155979
loren klingman <loren@klingman.us>
2017-03-12 20:31:43 -0400

    failing test

--- js/book.controller.spec.js
+++ js/book.controller.spec.js
@@ -9,12 +9,10 @@ describe('Book controller', function() {
   }));
 
   it('has a dummy spec to test 2 + 2', function() {
-    // An intentionally failing test. No code within expect() will never equal 4.
     expect(2+2).toEqual(4);
   });
   it('has a dummy spec to test 3 + 2', function() {
-    // An intentionally failing test. No code within expect() will never equal 4.
-    expect(3+2).toEqual(5);
+    expect(3+2).toEqual(6);
   });
 
   describe('$scope.name', function() {
